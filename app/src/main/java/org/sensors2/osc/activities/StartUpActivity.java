492fbece8e803c9d5cb7671170301c2190f879f9
vishalBindal <vishalbindal.06@gmail.com>
2019-06-06 14:21:29 +0530

    Removed guide,about activities and set orientation to portrait

--- app/src/main/java/org/sensors2/osc/activities/StartUpActivity.java
+++ app/src/main/java/org/sensors2/osc/activities/StartUpActivity.java
@@ -83,6 +83,7 @@ public class StartUpActivity extends FragmentActivity implements SensorActivity,
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         this.settings = this.loadSettings();
         this.dispatcher = new OscDispatcher();
         this.sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
@@ -328,22 +329,10 @@ public class StartUpActivity extends FragmentActivity implements SensorActivity,
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        switch (id) {
-            case R.id.action_settings: {
+        if(id==R.id.action_settings) {
                 Intent intent = new Intent(this, SettingsActivity.class);
                 startActivity(intent);
                 return true;
-            }
-            /*case R.id.action_guide: {
-                Intent intent = new Intent(this, GuideActivity.class);
-                startActivity(intent);
-                return true;
-            }
-            case R.id.action_about: {
-                Intent intent = new Intent(this, AboutActivity.class);
-                startActivity(intent);
-                return true;
-            }*/
         }
         return super.onOptionsItemSelected(item);
     }
@@ -407,11 +396,9 @@ public class StartUpActivity extends FragmentActivity implements SensorActivity,
                 this.wakeLock.acquire();
             }
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-            this.setRequestedOrientation(this.getCurrentOrientation());
         } else {
             this.wakeLock.release();
             getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-            this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
         }
         active = isChecked;
     }
@@ -441,51 +428,7 @@ public class StartUpActivity extends FragmentActivity implements SensorActivity,
         return false;
     }
 
-    public int getCurrentOrientation() {
 
-        final Display display = this.getWindowManager().getDefaultDisplay();
-        final int width, height;
-        if (Build.VERSION.SDK_INT >= 13) {
-            Point size = new Point();
-            display.getSize(size);
-            width = size.x;
-            height = size.y;
-        }
-        else {
-            width = display.getWidth();
-            height = display.getHeight();
-        }
-        switch(display.getRotation()){
-            case Surface.ROTATION_90:
-                if (width > height) {
-                    return ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
-                }
-                else {
-                    return ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
-                }
-            case Surface.ROTATION_180:
-                if (height > width) {
-                    return ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
-                }
-                else {
-                    return ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
-                }
-            case Surface.ROTATION_270:
-                if (width > height) {
-                    return ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
-                }
-                else {
-                    return ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
-                }
-            default:
-                if (height > width) {
-                    return ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
-                }
-                else {
-                    return ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
-                }
-        }
-    }
 
     @Override
     public boolean dispatchKeyEvent(KeyEvent event) {
